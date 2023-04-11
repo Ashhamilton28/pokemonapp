db7297795a5d6d1027295602357b405d31c9b079
Ashley <Ashhamilton22888@gmail.com>
2023-04-11 17:03:22 -0400

    My server is set up and running

---
+++ server.js
@@ -0,0 +1,54 @@
+//call and configure your dotenv package
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose')
+//data
+const pokemon = require('./models/pokemon')
+const app = express();
+const PORT = 3000
+
+
+//====Configuration
+//these 2 lines of code are needed to set everything up
+app.set('view engine', 'jsx')
+
+app.engine('jsx', require('jsx-view-engine').createEngine())
+
+//======Middleware
+//Setting a middleware to run in our app
+app.use((req, res, next) => {
+
+    console.log(req.url)
+    next()
+})
+
+//parses the data from the request
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res)=>{
+    res.send(pokemon)
+})
+
+
+//if none of the routes matches the request show 404 page
+app.get('*', (req, res) => {
+    res.redirect('404')
+})
+
+
+
+
+// Tell the app to listen on port 3000
+app.listen(3000, () => {
+    console.log(`Server running on  port: ${PORT}`);
+    mongoose.set('strictQuery', true)
+    // connect to mongodDB
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    mongoose.connection.once('open', () => {
+        console.log('Connected to MongoDB!')
+    })
+})
+
