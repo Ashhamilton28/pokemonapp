94ecf7a06e733480f9cc01ad5fefcd5c4ded0b61
Ashley <Ashhamilton22888@gmail.com>
2023-04-13 08:55:49 -0400

    Created show views of each pokemon

--- server.js
+++ server.js
@@ -35,13 +35,13 @@ app.get('/pokemon', (req, res)=>{
     res.render('Index', {pokemon: pokemon})
 })
 
-// app.get('/pokemon/:indexOfPokemonArray', (req,res)=>{
-//     res.render('Show',{pokemon: pokemon[req.params.indexOfPokemonArray]})
-// })
-
-app.get('/pokemon/:id',(req, res)=>{
-    res.send (req.params.id)
+app.get('/pokemon/:id', (req,res)=>{
+    res.render('Show',{pokemon: pokemon[req.params.id]})
 })
+
+// app.get('/pokemon/:id',(req, res)=>{
+//     res.send (req.params.id)
+// })
 
 //if none of the routes matches the request show 404 page
 app.get('*', (req, res) => {
@@ -56,7 +56,7 @@ app.listen(3000, () => {
     console.log(`Server running on  port: ${PORT}`);
     mongoose.set('strictQuery', true)
     // connect to mongodDB
-    mongoose.connect(process.env.MONGO_URI, {
+    mongoose.connect(process.env.MONGO_URI_KEY, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
--- views/Show.jsx
+++ views/Show.jsx
@@ -6,11 +6,12 @@ function Show(props){
     console.log(pokemon)
     return (
         <div>
-            <h1>Pokemon Show Page</h1>
+            <h1>Gotta Catch 'Em All</h1>
 
             <h2>{pokemon.name} 
             </h2>
             <img src={pokemon.img} href="/"/>
+            <div><a href="/">Back</a></div>
         </div>
     )
 }
